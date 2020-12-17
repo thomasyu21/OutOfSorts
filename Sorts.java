3c60eb61ed4c77f63b3611cb1bd315b319d2ce09
thomasyu21 <thomasyu085@gmail.com>
2020-12-16 21:27:47 -0500

    Created a Tester class to test my bubbleSort method. Debugged errors with bubbleSort.

--- Sorts.java
+++ Sorts.java
@@ -5,8 +5,8 @@ public class Sorts{
     int numSwap = 0;
     for (int i = 0; i < numLoop; numLoop--){
       numLoop2 = numLoop;
+      numSwap = 0;
       for (int j = 0; j < numLoop2; j++){
-        numSwap = 0;
         int temp1 = data[j];
         int temp2 = data[j+1];
         if (temp1 > temp2){
@@ -14,9 +14,9 @@ public class Sorts{
           data[j] = temp2;
           numSwap++;
         }
-        if (numSwap == 0){
-          i = numLoop;
-        }
+      }
+      if (numSwap == 0){
+        i = numLoop;
       }
     }
   }
---
+++ Tester.java
@@ -0,0 +1,36 @@
+import java.util.*;
+public class Tester{
+  public static void main(String[] args){
+    int[] a = {10,9,8,7,6,5,4,3,2,1};
+    int[] b = {10,9,8,7,6,5,4,3,2,1};
+    Sorts.bubbleSort(a);
+    Arrays.sort(b);
+    if ((Arrays.toString(a)).equals(Arrays.toString(b))){
+    }else{
+      System.out.println("Failed to sort backwards array");
+      System.out.println("Bubble Sort: " + Arrays.toString(a));
+      System.out.println("Correct: " + Arrays.toString(b));
+    }
+
+    int[] test = new int[20];
+    int[] correct = new int[20];
+
+    for (int i = 0; i < 10000; i++){
+      Random rng = new Random();
+      for(int j =  0; j < 20; j++){
+        int temp = (rng.nextInt() % 1000);
+        test[j] = temp;
+        correct[j] = temp;
+      }
+      Sorts.bubbleSort(test);
+      Arrays.sort(correct);
+      if ((Arrays.toString(test)).equals(Arrays.toString(correct))){
+      }else{
+          System.out.println("ERROR");
+          System.out.println("Bubble Sort: " + Arrays.toString(test));
+          System.out.println("Correct: " + Arrays.toString(correct));
+      }
+    }
+    System.out.println("All Good");
+  }
+}
