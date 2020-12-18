b8d20713360dc1ed06421ff76116511e34b9341c
thomasyu21 <thomasyu085@gmail.com>
2020-12-18 11:56:04 -0500

    Removed extra variables from bubbleSort method.

--- Sorts.java
+++ Sorts.java
@@ -1,22 +1,18 @@
 public class Sorts{
   public static void bubbleSort(int[] data){
-    int numLoop = data.length-1;
-    int numLoop2 = data.length-1;
     int numSwap = 0;
-    for (int i = 0; i < numLoop; numLoop--){
-      numLoop2 = numLoop;
+    for (int i = 0; i < data.length-1; i++){
       numSwap = 0;
-      for (int j = 0; j < numLoop2; j++){
-        int temp1 = data[j];
-        int temp2 = data[j+1];
-        if (temp1 > temp2){
-          data[j+1] = temp1;
-          data[j] = temp2;
+      for (int j = 0; j < data.length-1-i; j++){
+        int temp = data[j];
+        if (data[j] > data[j+1]){
+          data[j] = data[j+1];
+          data[j+1] = temp;
           numSwap++;
         }
       }
       if (numSwap == 0){
-        i = numLoop;
+        i = data.length;
       }
     }
   }
