7efc5676e886eabde8e6fd3b85155fc7d881d2eb
thomasyu21 <thomasyu085@gmail.com>
2020-12-22 12:14:21 -0500

    Wrote code to create the randData array for each option. Moved the location of the intialization of my temp variable into the if block to fix runtime.

--- Sorts.java
+++ Sorts.java
@@ -4,8 +4,8 @@ public class Sorts{
     for (int i = 0; i < data.length-1; i++){
       numSwap = 0;
       for (int j = 0; j < data.length-1-i; j++){
-        int temp = data[j];
         if (data[j] > data[j+1]){
+          int temp = data[j];
           data[j] = data[j+1];
           data[j+1] = temp;
           numSwap++;
